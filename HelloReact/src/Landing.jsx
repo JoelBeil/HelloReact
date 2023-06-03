8aedbc1424f2f25063e3941bb4482f1c2ccd1d8c
Joel Beil <joelbeil@yahoo.com>
2023-06-03 14:07:14 -0400

    Initial React test

---
+++ HelloReact/src/Landing.jsx
@@ -0,0 +1,9 @@
+export default function Landing() {
+    return (
+      <>
+        <h1>Joel S. Beil, Ph.D.</h1>
+        <h2>(330)357-7690</h2>
+        <h2>joelbeil@yahoo.com</h2>
+      </>
+    )
+  }
